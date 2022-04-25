0d0b05c00855a81e6102728b6fee9f1fbc912ed9
laurentravix <laurentravix@gmail.com>
2022-04-25 22:30:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { fetchProducts } from "./services/storefront";
+import { useState, useEffect } from "react";
+import Product from "./components/Product";
+import "./App.css";
 
 function App() {
+  const [productsList, setProductsList] = useState([]);
+  const [category, setCategory] = useState("electronics");
+
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const products = await fetchProducts(category);
+      setProductsList(products);
+    };
+    getProducts();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        <div className="title">
+          <h1>My Awesome Store</h1>
+        </div>
+
+        <select onChange={(e) => handleChange(e)}>
+          <option value="electronics">Electronics</option>
+          <option value="jewelery">Jewelery</option>
+          <option value="men's clothing">Men's clothing</option>
+          <option value="women's clothing">Women's clothing</option>
+        </select>
+
+        <div className="card-container">
+          {productsList.map((product, key) => (
+            <Product
+              key={product.id}
+              name={product.title}
+              image={product.image}
+              description={product.description}
+              price={product.price}
+            />
+          ))}
+        </div>
+        
+      </div>
     </div>
   );
 }
